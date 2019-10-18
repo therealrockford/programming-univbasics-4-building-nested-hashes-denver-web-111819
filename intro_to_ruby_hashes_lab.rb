982b3688f49624efb41793f07d4c8750b4df8924
Rocky Pitto <rockypitto@gmail.com>
2019-10-18 23:03:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,8 @@ def monopoly_with_second_tier
         :two_pieces_owned => 50,
         :three_pieces_owned => 100,
         :four_pieces_owned => 200
-      }
+      },
+      names:
       }
     }
   monopoly
