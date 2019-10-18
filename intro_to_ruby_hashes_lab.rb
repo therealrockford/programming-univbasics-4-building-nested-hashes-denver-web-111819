a779b5a1a93640939f699d265e1cd61e56d45a94
Rocky Pitto <rockypitto@gmail.com>
2019-10-18 22:56:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,9 +8,12 @@ end
 def monopoly_with_second_tier
    monopoly = {
     :railroads => {
-      pieces: 4
+      pieces: 4,
       :rent_in_dollars => {
-        one_piece_owned: 25
+        one_piece_owned: 25,
+        two_pieces_owned: 50,
+        three_pieces_owned: 100,
+        
       }
       }
     }
