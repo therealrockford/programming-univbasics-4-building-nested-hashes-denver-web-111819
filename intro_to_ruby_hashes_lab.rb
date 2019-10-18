e649683d62e07f95dfa563032d2a8191b6e8c79c
Rocky Pitto <rockypitto@gmail.com>
2019-10-18 23:05:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,11 +18,11 @@ def monopoly_with_second_tier
         :four_pieces_owned => 200
       },
       :names => 
-      {
-        :reading_railroad=>
-      {
-        "mortgage_value"=>"$100"
-      },
+        {
+         :reading_railroad=>
+        {
+         "mortgage_value"=>"$100"
+        },
       }
       }
       }
